81154c1ca7ab1450b33ebd69a5fd2680eb0ec8f5
gcc2ge <wushouhe@hotmail.com>
2016-12-30 20:43:53 +0800

    for select loop

---
+++ main/syncAndSubscribe.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/ethereum/go-ethereum/rpc"
+	"zhiwang_bc_message/geth/json"
+	"zhiwang_bc_message/geth/subscribe"
+	//"zhiwang_bc_message/geth/utils"
+	"fmt"
+)
+
+func main() {
+	client, _ := rpc.Dial("http://172.16.10.163:8545")
+	blockChan := make(chan *json.JsonHeader)
+	subscribe.SyncAndSubscribBlock(client, blockChan)
+	for {
+		select {
+		case block := <-blockChan:
+		//utils.PrintBlock(block)
+			if block != nil {
+				fmt.Printf("%v \n", block.Number)
+
+			}else{
+				fmt.Println("nil block")
+			}
+		}
+	}
+
+}
