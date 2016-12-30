212ae43f0b847fa5b97f12421b82c43e033e5832
gcc2ge <wushouhe@hotmail.com>
2016-12-30 20:44:35 +0800

    for select loop

--- geth/subscribe/sync.go
+++ geth/subscribe/sync.go
@@ -8,11 +8,18 @@ import (
 	"fmt"
 	"math/big"
 )
-
+//同步区块
 func SyncBlock(client *rpc.Client, blockChan chan *json.JsonHeader, from, to int64) {
 	for i := from; i < to; i++ {
 		go func(num int64) {
-			blockChan <- getBlockByNumber(client, big.NewInt(num))
+			Loop:
+			for {
+				select {
+				case blockChan <- getBlockByNumber(client, big.NewInt(num)):
+					break Loop
+				}
+			}
+
 		}(i)
 	}
 }
@@ -33,7 +40,6 @@ func toBlockNumArg(number *big.Int) string {
 	return fmt.Sprintf("%#x", number)
 }
 
-//chan
 func getLastBlock(client *rpc.Client, blockChan chan *json.JsonHeader) *big.Int {
 	var block json.JsonHeader = json.JsonHeader{}
 	if err := client.CallContext(context.Background(), &block, "eth_getBlockByNumber", toBlockNumArg(nil), true); err != nil {
@@ -42,12 +48,30 @@ func getLastBlock(client *rpc.Client, blockChan chan *json.JsonHeader) *big.Int 
 	}
 
 	go func() {
-		blockChan <- &block
+		Loop:
+		for {
+			select {
+			case blockChan <- &block:
+				break Loop
+			}
+		}
+
 	}()
 
 	return block.Number.ToInt()
 }
 
-func checkSyncBlock() {
+/**
+	同步所有数据
+	问题：1，订阅之前获取最新区块，会漏取区块
+	     2，订阅之后获取最新区块链 会多取区块（解决：插入时候去重）
+ */
+func SyncAndSubscribBlock(client *rpc.Client, blockChan chan *json.JsonHeader) {
 
+	//subscribe
+	ListenNewBlock(client, blockChan)
+	//订阅之后last blcok
+	lastBlockNum := getLastBlock(client, blockChan)
+	//sync
+	SyncBlock(client, blockChan, 0, lastBlockNum.Int64())
 }
